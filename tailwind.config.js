1bfe20b3e493d88576dc6c9c24ec26a9b9e45337
mh3mh0 <jonxca@icloud.com>
2021-05-13 10:01:20 +0200

    dos

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f3c614",
+        secondary: "#353535",
+        third: "#f8c733",
+        fourth: "#fdf0c7",
+      },
+      height: {
+        80: "20rem",
+        100: "25rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      padding: ["hover"],
+    },
+  },
+  plugins: [],
+};
